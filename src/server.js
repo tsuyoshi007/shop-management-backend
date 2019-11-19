cc610ec08db6286c000b3ed8b28844da9e54dee7
tsuyoshi007 <hunvikran18@kit.edu.kh>
2019-11-19 22:10:11 +0700

    :tada: Initial Commit

---
+++ src/server.js
@@ -0,0 +1,66 @@
+require('dotenv').config()
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+const DB_HOST = process.env.DB_HOST;
+const express = require('express')
+
+// Express Middleware
+const helmet = require('helmet') // creates headers that protect from attacks (security)
+const bodyParser = require('body-parser') // turns response into usable format
+const cors = require('cors')  // allows/disallows cross-site communication
+const morgan = require('morgan') // logs requests
+
+// db Connection w/ Heroku
+// const db = require('knex')({
+//   client: 'pg',
+//   connection: {
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: true,
+//   }
+// });
+
+// db Connection w/ localhost
+var db = require('knex')({
+    client: 'pg',
+    connection: {
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: DB_NAME
+    }
+});
+
+// Controllers - aka, the db queries
+const main = require('./controller')
+
+// App
+const app = express()
+
+// App Middleware
+const whitelist = ['http://localhost:3001']
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+app.use(helmet())
+app.use(cors(corsOptions))
+app.use(bodyParser.json())
+app.use(morgan('combined')) // use 'tiny' or 'combined'
+
+// App Routes - Auth
+app.get('/', (req, res) => res.send('hello world'))
+app.get('/crud', (req, res) => main.getTableData(req, res, db))
+app.post('/crud', (req, res) => main.postTableData(req, res, db))
+app.put('/crud', (req, res) => main.putTableData(req, res, db))
+app.delete('/crud', (req, res) => main.deleteTableData(req, res, db))
+
+// App Server Connection
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT || 3000}`)
+})
